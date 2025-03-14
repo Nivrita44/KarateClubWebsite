d7ed1a29b51efcab2f5b73573a4f4aea316f5a07
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2025-03-14 15:50:22 +0600

    Add Navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my-profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
